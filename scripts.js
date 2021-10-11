9ab1c54c148b37c7914a8fca30e9ad5855313d4f
Craig <84283410+cshields1@users.noreply.github.com>
2021-10-11 11:21:32 -0700

    Game end logic

--- scripts.js
+++ scripts.js
@@ -1,30 +1,32 @@
 const Board = (() => {
-  const board = ["", "", "", "", "", "", "", "", ""];
+  const board = [[], [], []];
+  const display = document.querySelector("#display");
   const drawBoard = () => {
-    const display = document.querySelector("#display");
     for (let i = 0; i < 9; i++) {
       const square = document.createElement("div");
       square.classList.add("square", `square-${i + 1}`);
       square.setAttribute("is-clickable", true);
       square.addEventListener("click", () => {
-        if (square.hasAttribute("is-clickable")) {
-          square.removeAttribute("is-clickable");
-          if (Game.isXTurn) {
-            square.textContent = "X";
-            Game.isXTurn = false;
-          } else {
-            square.textContent = "O";
-            Game.isXTurn = true;
-          }
+        square.removeAttribute("is-clickable");
+        if (Game.isXTurn) {
+          square.textContent = "X";
+          board[i] = "X";
+          Game.isXTurn = false;
+        } else {
+          square.textContent = "O";
+          board[i] = "O";
+          Game.isXTurn = true;
         }
+        Game.checkGameStatus();
       });
-      square.textContent = board[i];
       display.append(square);
     }
   };
 
-  return { board, drawBoard };
+  return { display, board, drawBoard };
 })();
+
+Board.drawBoard();
 
 const Player = (name, catchphrase) => {
   let wins = 0;
@@ -70,5 +72,54 @@ const Game = (() => {
 
   let isXTurn = true;
 
-  return { resetPlayers, isXTurn };
+  let isGameOver = false;
+
+  const checkGameStatus = () => {
+    const squares = Board.board;
+
+    const rows = [
+      [squares[0], squares[1], squares[2]],
+      [squares[3], squares[4], squares[5]],
+      [squares[6], squares[7], squares[8]],
+    ];
+    const columns = [
+      [squares[0], squares[3], squares[6]],
+      [squares[1], squares[4], squares[7]],
+      [squares[2], squares[5], squares[8]],
+    ];
+    const diagonals = [
+      [squares[0], squares[4], squares[8]],
+      [squares[2], squares[4], squares[6]],
+    ];
+
+    for (let row of rows) {
+      if (
+        row.every((square) => square === "X") ||
+        row.every((square) => square === "O")
+      ) {
+        isGameOver = true;
+        alert("GAME OVER!");
+      }
+    }
+    for (let column of columns) {
+      if (
+        column.every((square) => square === "X") ||
+        column.every((square) => square === "O")
+      ) {
+        isGameOver = true;
+        alert("GAME OVER!");
+      }
+    }
+    for (let diagonal of diagonals) {
+      if (
+        diagonal.every((square) => square === "X") ||
+        diagonal.every((square) => square === "O")
+      ) {
+        isGameOver = true;
+        alert("GAME OVER!");
+      }
+    }
+  };
+
+  return { resetPlayers, isXTurn, checkGameStatus };
 })();
